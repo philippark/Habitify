abb9b6c942559e2172b1743a0db88d0490b4a2a7
philippark <philippark271@gmail.com>
2024-03-23 22:36:04 -0400

    different shades of green

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Box from './Box.js'
 import BoxList from './BoxList.js'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 function App() {
-  const [boxColor, setboxColor] = useState("green");
+  const colors = ["#4B6F44", "#32de84", "#4FFFB0"];
+  let [index, setIndex] = useState(0);
+
+  const [boxColor, setboxColor] = useState(colors[index]);
 
   const clickHandle = ()=>{
-    setboxColor("red");
-    console.log(boxColor);
+    setIndex(index += 1);
+    setboxColor(colors[index]);
+
   }
+
+  useEffect(()=>{
+    console.log(index);
+  })
 
   return (
     <div className="App">
